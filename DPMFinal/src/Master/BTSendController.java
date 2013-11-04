9bbcb34a4a5de49d75c632e211232ea9418218ea
mattgray7 <weeman212@vpn157150.wireless.mcgill.ca>
2013-11-04 11:23:06 -0800

    Added first BlueTooth connection code

--- DPMFinal/src/Master/BTSendController.java
+++ DPMFinal/src/Master/BTSendController.java
@@ -1,10 +1,14 @@
 package Master;
 
-import lejos.nxt.ColorSensor;
-import lejos.nxt.NXTRegulatedMotor;
-import lejos.nxt.SensorPort;
-import lejos.nxt.UltrasonicSensor;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import javax.bluetooth.RemoteDevice;
+
 import lejos.nxt.*;
+import lejos.nxt.comm.BTConnection;
+import lejos.nxt.comm.Bluetooth;
 
 
 public class BTSendController {
@@ -15,6 +19,8 @@ public class BTSendController {
 	public static Navigation nav = new Navigation(odo);
 	public static LCDInfo lc = new LCDInfo(odo);
 	public static Localization usl = new Localization(odo);
+	
+	private static BTConnection connection;
 	
 	private NXTRegulatedMotor leftMotor;
 	private NXTRegulatedMotor rightMotor;
@@ -32,26 +38,68 @@ public class BTSendController {
 		nav.travelTo(0.0,0.0,false);
 		
 		
+		
+		connection.close();
 	}
 	
 	
 	//establish initial connection, returns true once connection established - NEED TO BE BOOLEAN??
 	public Boolean establishConnection(){
+		String name = "slave2";		//friendly name of other brick
 		
-		return false;
+		try{
+			//make the connection
+			RemoteDevice receiver = Bluetooth.getKnownDevice(name);
+			connection = Bluetooth.connect(receiver);
+			
+			//wont continue until reciever is connected
+			while(receiver == null){
+				receiver = Bluetooth.getKnownDevice(name);
+			}
+
+			
+			LCD.drawString("connected.", 0, 1);
+			return true;
+
+		} catch(Exception ioe){
+			LCD.clear();
+			LCD.drawString("ERROR", 0, 0);
+			LCD.drawString(ioe.getMessage(), 0, 2);
+			LCD.refresh();
+			return false;
+		}
+		
 	}
 	
 	
 	//sends signal to slave brick
-	public void sendSignal(int signal){
-		
+	public void sendSignal(int signal) throws IOException{
+		DataOutputStream output = connection.openDataOutputStream();
+		DataInputStream input = connection.openDataInputStream();
+		try
+		{
+			output.writeInt(signal);
+			output.flush();
+			int reply = input.readInt();	//blocking, will wait for reply signal (hopefully)
+			input.close();
+			output.close();
+			
+			
+		}
+		catch(Exception ioe)
+		{
+			LCD.drawString("Could not send signal", 0, 0, false);
+		}
+		output.close();
 	}
 	
 	
 	//called when the slave brick is operating, will pause execution until "finished" signal received
-	public void slaveFlow(){
+	/*public void slaveFlow(){
+		DataInputStream input = connection.openDataInputStream();
+		int reply = input.readInt();
 		
-	}
+	}*/
 	
 	
 }
