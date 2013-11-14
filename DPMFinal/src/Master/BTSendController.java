6c68ea48901bca3d3be1774571fdcf52c6dc0d12
mattgray7 <matthew.gray@mail.mcgill.ca>
2013-11-14 08:17:12 -0800

    Updated controller

--- DPMFinal/src/Master/BTSendController.java
+++ DPMFinal/src/Master/BTSendController.java
@@ -9,14 +9,15 @@ import javax.bluetooth.RemoteDevice;
 import lejos.nxt.*;
 import lejos.nxt.comm.BTConnection;
 import lejos.nxt.comm.Bluetooth;
+import Master.Odometer;
 
 
 public class BTSendController {
 	
 	public static UltrasonicSensor bottomUs = new UltrasonicSensor(SensorPort.S1);
-	public static ColorSensor odometerSensor= new ColorSensor(SensorPort.S2);
-	public static ColorSensor objectSensor = new ColorSensor(SensorPort.S3);
-	public static UltrasonicSensor topUs = new UltrasonicSensor(SensorPort.S4);
+	public static ColorSensor odoSensor= new ColorSensor(SensorPort.S2);
+	public static ColorSensor colorSensor = new ColorSensor(SensorPort.S3);
+	//public static UltrasonicSensor topUs = new UltrasonicSensor(SensorPort.S4);
 	
 	
 	public static final int DEFAULT_PERIOD = 25;
@@ -24,17 +25,17 @@ public class BTSendController {
 	public static Odometer odo = new Odometer(2.67, 2.668, 17.4);
 	public static BTSend bts = new BTSend();
 	
-	public static Navigation nav = new Navigation(odo, bts, topUs, bottomUs, objectSensor);
-	public static OdometryCorrection oc = new OdometryCorrection(odo, odometerSensor, nav);
+	public static Navigation nav = new Navigation(odo, bts, bottomUs, colorSensor);
+	public static OdometryCorrection oc = new OdometryCorrection(odo, odoSensor, nav);
 
 	public static LCDInfo lc = new LCDInfo(odo);
-	public static Localization usl = new Localization(odo, bottomUs);
+	public static Localization usl = new Localization(odo, bottomUs, nav, odoSensor);
 	
 
 	
 	private NXTRegulatedMotor leftMotor = Motor.B;
 	private NXTRegulatedMotor rightMotor = Motor.C;
-	private NXTRegulatedMotor sensorMotor;	//if we need a sensor motor
+	private NXTRegulatedMotor sensorMotor = Motor.A;	//if we need a sensor motor
 	
 
 	//TouchSensor touchSensor = new TouchSensor(SensorPort.S4);
@@ -46,12 +47,14 @@ public class BTSendController {
 	 */
 	public static void main(String[] args){
 		int buttonChoice = Button.waitForAnyPress();
-		//usl.doLocalization();
-		//nav.turnTo(90.0, true);
 		bts.establishConnection();
+		odo.start();
+		usl.doLocalization();
+		usl.doLightLocalization();
+
 
 		//oc.start();
-		odo.start();
+
 		nav.start();
 
 		
