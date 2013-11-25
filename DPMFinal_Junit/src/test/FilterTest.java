ebc8d602bd3885143abed139fa21815dd74611ae
Nicholas Aird <nicholas.aird@videotron.ca>
2013-11-25 11:19:01 -0500

    Ok, I forgot to include the unit testing.
    Here it is.

--- DPMFinal_Junit/src/test/FilterTest.java
+++ DPMFinal_Junit/src/test/FilterTest.java
@@ -13,28 +13,66 @@ public class FilterTest {
 	// Test case where we haven't added any samples.
 	///////////////////////////////
 	@Test
-	public void testNoSamples() {
-		Filter t = new Filter(3);
+	public void testNoSamplesFiltered() {
+		Filter f = new Filter(3);
 		
-		assertTrue(t.getFilteredValue() == 0);
+		assertTrue(f.getFilteredValue() == 0);
 	}
 
-	///////////////////////////////
-	// Test base case
-	///////////////////////////////
 	@Test
-	public void testAllZeros() {
-		Filter t = new Filter(3);
+	public void testNoSamplesRaw(){
+		Filter f = new Filter(3);
 		
-		t.add(0);
-		t.add(0);
-		t.add(0);
-		
-		assertTrue(t.getFilteredValue() == 0);
+		assertTrue(f.getRawValue() == 0);
 	}
 	
 	///////////////////////////////
-	// Test that only the last value is returned.
+	// Test that getRawValue() returns the last raw value
+	///////////////////////////////
+	@Test
+	public void testRawValueB(){
+		Filter f = new Filter(3);
+		
+		f.add(1);
+		
+		assertTrue(f.getRawValue() == 1);
+	}
+
+	@Test
+	public void testRawValueC(){
+		Filter f = new Filter(3);
+		
+		f.add(1);
+		f.add(2);
+		
+		assertTrue(f.getRawValue() == 2);
+	}
+
+	@Test
+	public void testRawValueD(){
+		Filter f = new Filter(3);
+
+		f.add(1);
+		f.add(2);
+		f.add(3);
+		
+		assertTrue(f.getRawValue() == 3);
+	}
+
+	@Test
+	public void testRawValueE(){
+		Filter f = new Filter(3);
+
+		f.add(1);
+		f.add(2);
+		f.add(3);
+		f.add(4);
+		
+		assertTrue(f.getRawValue() == 4);
+	}
+	
+	///////////////////////////////
+	// Test that only the last filtered value is returned.
 	///////////////////////////////
 	@Test
 	public void testLastValueA() {
@@ -81,7 +119,7 @@ public class FilterTest {
 	// Test with larger size.
 	///////////////////////////////
 	@Test
-	public void testLargeSize() {
+	public void testLargeSizeA() {
 		Filter t = new Filter(10);
 		
 		t.add(1);
@@ -90,13 +128,24 @@ public class FilterTest {
 		
 		assertTrue(t.getFilteredValue() == 4);
 	}
+	
+	@Test
+	public void testLargeSizeB() {
+		Filter t = new Filter(10);
+		
+		t.add(1);
+		t.add(2);
+		t.add(4);
+		
+		assertTrue(t.getRawValue() == 4);
+	}
 
 	///////////////////////////////
 	// Test that calling getFilteredValue multiple times does not
 	// break anything.
 	///////////////////////////////
 	@Test
-	public void testMultipleQueries() {
+	public void testMultipleQueriesFiltered() {
 		Filter t = new Filter(10);
 		
 		t.add(1);
@@ -107,5 +156,23 @@ public class FilterTest {
 		t.getFilteredValue();
 		
 		assertTrue(t.getFilteredValue() == 4);
+	}
+
+	///////////////////////////////
+	// Test that calling getRawValue multiple times does not
+	// break anything.
+	///////////////////////////////
+	@Test
+	public void testMultipleQueriesRaw() {
+		Filter t = new Filter(10);
+		
+		t.add(1);
+		t.add(2);
+		t.add(4);
+		
+		t.getRawValue();
+		t.getRawValue();
+		
+		assertTrue(t.getRawValue() == 4);
 	}
 }
